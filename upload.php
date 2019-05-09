d1110ffd9fdfb1a2f0011709fa0411ab8e56e1bd
Nurharis Dwi Atmojo <nurharis@student.uns.ac.id>
2019-05-09 23:34:28 +0700

    tugas gdrive K3516044

---
+++ upload.php
@@ -0,0 +1,24 @@
+<?php
+   if(isset($_FILES['image'])){
+      $errors= array();
+      $file_name = $_FILES['image']['name'];
+      $file_tmp =$_FILES['image']['tmp_name'];
+      $extensions= array("jpeg","jpg","png");
+      if(empty($errors)==true){
+         move_uploaded_file($file_tmp,"files/".$file_name);
+         header('Location: /sister');
+      }else{
+         print_r($errors);
+      }
+   }
+?>
+<html>
+   <body>
+      
+      <form action="" method="POST" enctype="multipart/form-data">
+         <input type="file" name="image" />
+         <input type="submit"/>
+      </form>
+      
+   </body>
+</html>
